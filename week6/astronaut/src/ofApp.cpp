b246fca759eccbbb575178f9975ba90fb07519e3
yue lin(Christine) <Christine@Christine-MacB.fios-router.home>
2016-03-11 10:52:46 -0500

    collision detect

--- week6/astronaut/src/ofApp.cpp
+++ week6/astronaut/src/ofApp.cpp
@@ -28,75 +28,82 @@ void ofApp::update(){
     spriteRenderer->clear();
     spriteRenderer->update(ofGetElapsedTimeMillis());
     
-    
     for(int i = 0; i < backgrounds.size(); i++){
-        //        spriteRenderer->addTile(backgrounds[i]->tileName, 0, backgrounds[i]->pos.x, backgrounds[i]->pos.y);
         spriteRenderer->addCenteredTile(backgrounds[i]->tileName, 0, backgrounds[i]->pos.x, backgrounds[i]->pos.y, 0, 1, 1, F_NONE, SCALE);
+//        spriteRenderer->addTile(backgrounds[i]->tileName, 0, backgrounds[i]->pos.x, backgrounds[i]->pos.y, 0, SCALE, SCALE, F_NONE);
     }
+    
     
     spriteRenderer->addCenteredTile(&player->animation, player->pos.x, player->pos.y, 1, F_NONE, SCALE);
+//    spriteRenderer->addTile(&player->animation, player->pos.x, player->pos.y, 1, F_NONE);
     
-    
-    //    bool addCenteredTile(animation_t* sprite, float x, float y, int layer = -1,                        flipDirection f = F_NONE, float scale = 1.0, int r=255, int g=255, int b=255, int alpha=255);
-    
-    
-    //if (world.isPlains[player.xPos][player.yPos-1] == true) {
-    //    player.yPos -=1;
-    //}
-    
-    
-    
-    
-    //if the tile int the player's next position is not a index 11, the player can keep moving
-    
-//        if (leftPressed) {
-//            if(backgrounds[(player->pos.x * player->pos.y) -1] == ){
-//                player->pos.x -= 4;
-//            }
-//            player->animation.index = 4;
+        if (leftPressed) {
+            player->animation.index = 4;
+//                backgrounds[floor(((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE)))
+//                                    * 50
+//                                    + floor(((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2)/ (spriteRenderer->getTileSize() * SCALE)))
+//                                    - 1]->tileName = floor(ofRandom(12));
+//
+
+            if(backgrounds[floor(((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE) ) )
+                           * 50
+                           + floor(((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2)/ (spriteRenderer->getTileSize() * SCALE) ) )
+                           - 1]->tileName != 11){
+                player->pos.x -= 4;
+            }
+        }
+        if (rightPressed) {
+            player->animation.index = 2;
+            if(backgrounds[floor(((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE) ) )
+                           * 50
+                           + floor(((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2)/ (spriteRenderer->getTileSize() * SCALE) ) )
+                           + 1]->tileName != 11){
+                player->pos.x += 4;
+            }
+        }
+        if (upPressed) {
+            player->animation.index = 0;
+            if(backgrounds[(floor(((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE) ) )
+                           -1)* 50
+                           + floor(((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2)/ (spriteRenderer->getTileSize() * SCALE) ) )
+                           ]->tileName != 11){
+                player->pos.y -= 4;
+            }
+        }
+        if (downPressed) {
+            player->animation.index = 6;
+            if(backgrounds[(floor(((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE) ) )
+                           +1)* 50
+                           + floor(((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2)/ (spriteRenderer->getTileSize() * SCALE) ) )
+                           ]->tileName != 11){
+                player->pos.y += 4;
+            }
+        }
 //    
-//        }
-//        if (rightPressed) {
-//    
-//            if(){
-//    
-//            }
-//             player->pos.x += 4;
-//            player->animation.index = 2;
-//        }
-//        if (upPressed) {
-//    
-//            if(){
-//                player->pos.y -= 4;
-//            }
-//            player->animation.index = 0;
-//        }
-//        if (downPressed) {
-//    
-//            if(){
-//                player->pos.y += 4;
-//            }
-//            player->animation.index = 6;
-//        }
+//    cout << backgrounds[floor(((player->pos.y + (spriteRenderer->getTileSize() * SCALE)/2) / (spriteRenderer->getTileSize() * SCALE)))
+//                        * 50
+//                        + floor(((player->pos.x + (spriteRenderer->getTileSize() * SCALE)/2)/ (spriteRenderer->getTileSize() * SCALE) ) )
+//                        - 1]->tileName <<endl;
     
     
-    if (leftPressed) {
-        player->pos.x -= 4;
-        player->animation.index = 4;
-        
-    }
-    if (rightPressed) {
-        player->pos.x += 4;
-        player->animation.index = 2;
-    }
-    if (upPressed) {
-        player->pos.y -= 4;
-        player->animation.index = 0;
-    }
-    if (downPressed) {
-        player->pos.y += 4;
-        player->animation.index = 6;
-    }
+//
+//    if (leftPressed) {
+//        player->pos.x -= 4;
+//        player->animation.index = 4;
+//        
+//    }
+//    if (rightPressed) {
+//        player->pos.x += 4;
+//        player->animation.index = 2;
+//    }
+//    if (upPressed) {
+//        player->pos.y -= 4;
+//        player->animation.index = 0;
+//    }
+//    if (downPressed) {
+//        player->pos.y += 4;
+//        player->animation.index = 6;
+//    }
     
 }
 
